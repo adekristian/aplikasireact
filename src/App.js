a52f7455eb2a3e6c4b378a0edd24f43ca4d5832f
adekristian <krsitianade098@gmail.com>
2020-07-09 01:33:35 +0700

    Routing pada React.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Component/Fungsional/Home';
+import HomePage from './Component/Fungsional/HomePage';
+import About from './Component/Fungsional/AboutComp';
 import Beranda from './Component/Class/Beranda';
 import Parent from './Component/Class/CSS/Parent';
 import Bootstrap from './Component/Class/Bootstrap';
-function App() {
+import { Navbar, NavbarBrand } from 'reactstrap';
+import NavbarComp from './Component/Fungsional/NavbarComp';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+const App =() => {
   return (
-    <div className="App">
-      <Bootstrap/>
+    <BrowserRouter>
+     <NavbarComp/>
+   <Switch>
+     <Route exact path="/" component ={HomePage} />
+     <Route exact path="/about" component ={About} />
+
+   </Switch>
+  
+    
+    
+    </BrowserRouter>
      
-    </div>
+     
+ 
   );
 }
 
